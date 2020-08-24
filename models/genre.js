59e8c637c1841abcc5f92978072acab49f4afd4e
Kocsis Róbert <robertzkocsis@gmail.com>
2020-08-24 16:48:25 +0200

    express tutorial part 1-3

---
+++ models/genre.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var GenreInstanceSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 }
+});
+
+GenreInstanceSchema
+  .virtual("url")
+  .get(() => {
+    return "/catalog/genre/" + this._id;
+  });
+
+module.exports = mongoose.model('GenreInstance', GenreInstanceSchema)
