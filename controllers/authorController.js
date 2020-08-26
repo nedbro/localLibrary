8fe24d6838fb29d9fe2cc698f437c8c359652d11
Kocsis Róbert <robertzkocsis@gmail.com>
2020-08-26 13:59:58 +0200

    tutorial part 6 wip

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 var Author = require("../models/author");
 var async = require("async");
 var Book = require("../models/book");
+const { body, validationResult, check } = require("express-validator");
 
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
@@ -48,23 +49,115 @@ exports.author_detail = (req, res) => {
 };
 
 // Display Author create form on GET.
-exports.author_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+exports.author_create_get = function(req, res, next) {
+  res.render("author_form", { title: "Create Author" });
 };
 
 // Handle Author create on POST.
-exports.author_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-};
+exports.author_create_post = [
+  body("first_name").isLength({ min: 1 }).trim().withMessage("First name must be specified")
+    .isAlphanumeric().withMessage("First name has non-alphanumeric characters."),
+
+  body("family_name").isLength({ min: 1 }).trim().withMessage("Family name must be specified."),
+  body("date_of_birth", "Invalid date of birth").optional({ checkFalsy: true }).isISO8601(),
+  body("date_of_death", "Invalid date of death").optional({ checkFalsy: true }).isISO8601(),
+
+  check("first_name").escape(),
+  check("family_name").escape(),
+  check("date_of_birth").toDate(),
+  check("date_of_death").toDate(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", { title: "Create author", author: req.body, errors: errors.array() });
+      return;
+    } else {
+      console.log("eljutok a kreálásig");
+      var author = new Author(
+        {
+          first_name: req.body.first_name,
+          family_name: req.body.family_name,
+          date_of_birth: req.body.date_of_birth,
+          date_of_death: req.body.date_of_death
+        }
+      );
+
+      author.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(author.url);
+      });
+    }
+  }
+
+];
 
 // Display Author delete form on GET.
-exports.author_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = function(req, res, next) {
+  async.parallel({
+      author: function(callback) {
+        console.log("callback1", callback);
+        Author.findById(req.params.id).exec(callback);
+      },
+      author_books: function(callback) {
+        console.log("callback2", callback);
+        Book.find({ "author": req.params.id }).exec(callback);
+      }
+    },
+    function(err, results) {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.author == null) {
+        res.redirect("/catalog/authors");
+      }
+
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.author_books
+      });
+    }
+  );
 };
 
 // Handle Author delete on POST.
-exports.author_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = (req, res, next) => {
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.body.authorid).exec(callback);
+    },
+    authors_books: function(callback) {
+      Book.find({ "author": req.body.authorid }).exec(callback);
+    }
+  }, function(err, results) {
+    if (err) {
+      return next(err);
+    }
+    // Success
+    if (results.authors_books.length > 0) {
+      // Author has books. Render in same way as for GET route.
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.authors_books
+      });
+      return;
+    } else {
+      // Author has no books. Delete object and redirect to the list of authors.
+      Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+        if (err) {
+          return next(err);
+        }
+        // Success - go to author list
+        res.redirect("/catalog/authors");
+      });
+    }
+  });
 };
 
 // Display Author update form on GET.
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,6 +6,8 @@ var Author = require("../models/author");
 var Genre = require("../models/genre");
 var BookInstance = require("../models/bookinstance");
 
+const { body, validationResult, check } = require("express-validator");
+
 exports.index = function(req, res) {
 
   async.parallel({
@@ -74,14 +76,99 @@ exports.book_detail = function(req, res, next) {
 };
 
 // Display book create form on GET.
-exports.book_create_get = function(req, res) {
-  res.send("NOT IMPLEMENTED: Book create GET");
+exports.book_create_get = function(req, res, next) {
+
+  // Get all authors and genres, which we can use for adding to our book.
+  async.parallel({
+    authors: function(callback) {
+      Author.find(callback);
+    },
+    genres: function(callback) {
+      Genre.find(callback);
+    }
+  }, function(err, results) {
+    if (err) {
+      return next(err);
+    }
+    res.render("book_form", { title: "Create Book", authors: results.authors, genres: results.genres });
+  });
+
 };
 
 // Handle book create on POST.
-exports.book_create_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: Book create POST");
-};
+exports.book_create_post = [
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === "undefined") {
+        req.body.genre = [];
+      } else {
+        req.body.genre = new Array(req.body.genre);
+      }
+    }
+    next();
+  },
+  body("title", "Title must not be empty").trim().isLength({ min: 1 }),
+  body("author", "Author must not be empty.").trim().isLength({ min: 1 }),
+  body("summary", "Summary must not be empty.").trim().isLength({ min: 1 }),
+  body("isbn", "ISBN must not be empty").trim().isLength({ min: 1 }),
+
+  check("*").escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    var book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre
+    });
+
+    if (!errors.isEmpty()) {
+
+      // Get all authors and genres for form.
+      async.parallel({
+          authors: function(callback) {
+            Author.find(callback);
+          },
+          genres: function(callback) {
+            Genre.find(callback);
+          }
+        },
+        function(err, results) {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected genres as checked.
+          for (let i = 0; i < results.genres.length; i++) {
+            if (book.genre.indexOf(results.genres[i]._id) > -1) {
+              results.genres[i].checked = "true";
+            }
+          }
+          res.render("book_form", {
+            title: "Create Book",
+            authors: results.authors,
+            genres: results.genres,
+            book: book,
+            errors: errors.array()
+          });
+        });
+      return;
+    } else {
+      // Data from form is valid. Save book.
+      book.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        //successful - redirect to new book record.
+        res.redirect(book.url);
+      });
+    }
+  }
+];
+
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
